984d82888f4010dcfc3bdcb154a337c80668a3ec
dhruvrana0 <200339616210@paruluniversity.ac.in>
2024-01-05 21:10:33 +0530

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,7 @@
-/**
- * Created by Rychou on 2018/4/19.
- */
+
 import React, { Component } from 'react'
 import $ from 'jquery'
-import url from './audio/omg.mp3' // 引入背景音乐文件
+import url from './audio/omg.mp3' 
 
 
 class Main extends Component {
@@ -13,32 +11,30 @@ class Main extends Component {
     componentDidMount() {
         this.print();
         setInterval(() => {
-            this.time(2023, 5, 17) // 你们的纪念日
+            this.time(2023, 5, 17) 
         }, 1000
         )
         var audio = document.getElementById("audio");
-        setTimeout(() => audio.play(), 8500) // 背景音乐在页面加载后，延迟播放的时长，单位：毫秒。
+        setTimeout(() => audio.play(), 8500) 
     }
     print = () => {
         $.fn.autotype = function () {
             var _this = $(this);
             var str = _this.html();
-            // 正则替换代码行之间添加的多个空格，不去除换行输出会有明显的停顿：实际是在输出多个空格
+            
             str = str.replace(/(\s){2,}/g, "$1");
             var index = 0;
             $(this).html('');
             var timer = function fn() {
                 var args = arguments;
                 var current = str.slice(index, index + 1);
-                // html标签完整输出,如：<p>
                 if (current == '<') {
                     index = str.indexOf('>', index) + 1;
                 }
                 else {
                     index++;
                 }
-                //位运算符: 根据setInterval运行奇偶次来判断是否加入下划线字符“_”，使输入效果更逼真
-                if (index < str.length - 1) { //打印字符倒数第2个字符开始，不加下划线字符，以防止结束符可能会多输出一下划线字符
+                if (index < str.length - 1) { 
                     _this.html(str.substring(0, index) + (index & 1 ? '_' : ''));
                 } else {
                     _this.html(str.substring(0, index));
@@ -46,7 +42,6 @@ class Main extends Component {
                 };
                 setTimeout(fn, 200)
             };
-            // 延迟1s开始
             setTimeout(timer, 1000);
         };
         $("#autotype").autotype();
